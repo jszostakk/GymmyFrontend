dcb4ee93f22d957b690a5f4df88a3fe99a1b6c7f
Damian <daba9@op.pl>
2023-11-16 22:42:24 +0100

    firebase/database added

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase/firebaseConfig";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const usersCollection = collection(db, 'users');
+
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const querySnapshot = await getDocs(usersCollection);
+      const usersData = [];
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data();
+        usersData.push({
+          id: doc.id,
+          name: userData.name,
+          surname: userData.surname,
+          age: userData.age,
+        });
+      });
+      setUsers(usersData);
+    };
+
+    getUsers();
+  })
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test!</Text>
+      <Text>Users:</Text>
+      {users.map((user) => (
+        <Text key={user.id}>
+          {`Name: ${user.name}, Surname: ${user.surname}, Age: ${user.age}`}
+        </Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0tbEhEBk116sLHHA3G1_JkVPQMzad5NM",
+    authDomain: "gymmy-gym.firebaseapp.com",
+    projectId: "gymmy-gym",
+    storageBucket: "gymmy-gym.appspot.com",
+    messagingSenderId: "142017426856",
+    appId: "1:142017426856:web:faeb9de3803bda8d04d99d",
+    measurementId: "G-SE3WM6KT6S"
+};
+
+export default firebaseConfig;
